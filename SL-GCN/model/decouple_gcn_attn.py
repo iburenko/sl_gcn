4ec68dab099cb9d90f3f8080c676c9b6dd27a5f0
Ilya Burenko <iburenko@gmail.com>
2023-09-13 09:58:04 +0200

    Adopt graph and model for Openpose25 keypoints

--- SL-GCN/model/decouple_gcn_attn.py
+++ SL-GCN/model/decouple_gcn_attn.py
@@ -1,3 +1,6 @@
+import sys
+import logging
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,6 +10,10 @@ import math
 from model.dropSke import DropBlock_Ske
 from model.dropT import DropBlockT_1d
 
+def LINE():
+    return sys._getframe(1).f_lineno
+
+logger = logging.getLogger(__name__)
 
 def import_class(name):
     components = name.split('.')
@@ -25,14 +32,14 @@ def conv_branch_init(conv):
     nn.init.constant(conv.bias, 0)
 
 
-def conv_init(conv):
-    nn.init.kaiming_normal(conv.weight, mode='fan_out')
-    nn.init.constant(conv.bias, 0)
+# def conv_init(conv):
+#     nn.init.kaiming_normal(conv.weight, mode='fan_out')
+#     nn.init.constant(conv.bias, 0)
 
 
-def bn_init(bn, scale):
-    nn.init.constant(bn.weight, scale)
-    nn.init.constant(bn.bias, 0)
+# def bn_init(bn, scale):
+#     nn.init.constant(bn.weight, scale)
+#     nn.init.constant(bn.bias, 0)
 
 
 class unit_tcn(nn.Module):
@@ -44,16 +51,25 @@ class unit_tcn(nn.Module):
 
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        conv_init(self.conv)
-        bn_init(self.bn, 1)
+        self.conv_init(self.conv)
+        self.bn_init(self.bn, 1)
 
         self.dropS = DropBlock_Ske(num_point=num_point)
         self.dropT = DropBlockT_1d(block_size=block_size)
 
     def forward(self, x, keep_prob, A):
-        x = self.bn(self.conv(x))
+        x = self.conv(x)
+        x = self.bn(x)
         x = self.dropT(self.dropS(x, keep_prob, A), keep_prob)
         return x
+    
+    def conv_init(self, conv):
+        nn.init.kaiming_normal_(conv.weight, mode='fan_out')
+        nn.init.constant_(conv.bias, 0)
+
+    def bn_init(self, bn, scale):
+        nn.init.constant_(bn.weight, scale)
+        nn.init.constant_(bn.bias, 0)
 
 
 class unit_tcn_skip(nn.Module):
@@ -65,12 +81,21 @@ class unit_tcn_skip(nn.Module):
 
         self.bn = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        conv_init(self.conv)
-        bn_init(self.bn, 1)
+        self.conv_init(self.conv)
+        self.bn_init(self.bn, 1)
 
     def forward(self, x):
-        x = self.bn(self.conv(x))
+        x = self.conv(x)
+        x = self.bn(x)
         return x
+    
+    def conv_init(self, conv):
+        nn.init.kaiming_normal_(conv.weight, mode='fan_out')
+        nn.init.constant_(conv.bias, 0)
+
+    def bn_init(self, bn, scale):
+        nn.init.constant_(bn.weight, scale)
+        nn.init.constant_(bn.bias, 0)
 
 
 class unit_gcn(nn.Module):
@@ -98,25 +123,33 @@ class unit_gcn(nn.Module):
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                conv_init(m)
+                self.conv_init(m)
             elif isinstance(m, nn.BatchNorm2d):
-                bn_init(m, 1)
-        bn_init(self.bn, 1e-6)
+                self.bn_init(m, 1)
+        self.bn_init(self.bn, 1e-6)
 
         self.Linear_weight = nn.Parameter(torch.zeros(
-            in_channels, out_channels * num_subset, requires_grad=True, device='cuda'), requires_grad=True)
+            in_channels, out_channels * num_subset, requires_grad=True), requires_grad=True)
         nn.init.normal_(self.Linear_weight, 0, math.sqrt(
             0.5 / (out_channels * num_subset)))
 
         self.Linear_bias = nn.Parameter(torch.zeros(
-            1, out_channels * num_subset, 1, 1, requires_grad=True, device='cuda'), requires_grad=True)
-        nn.init.constant(self.Linear_bias, 1e-6)
+            1, out_channels * num_subset, 1, 1, requires_grad=True), requires_grad=True)
+        nn.init.constant_(self.Linear_bias, 1e-6)
 
         eye_array = []
         for i in range(out_channels):
             eye_array.append(torch.eye(num_point))
         self.eyes = nn.Parameter(torch.tensor(torch.stack(
-            eye_array), requires_grad=False, device='cuda'), requires_grad=False)  # [c,25,25]
+            eye_array), requires_grad=False), requires_grad=False)  # [c,25,25]
+        
+    def conv_init(self, conv):
+        nn.init.kaiming_normal_(conv.weight, mode='fan_out')
+        nn.init.constant_(conv.bias, 0)
+
+    def bn_init(self, bn, scale):
+        nn.init.constant_(bn.weight, scale)
+        nn.init.constant_(bn.bias, 0)
 
     def norm(self, A):
         b, c, h, w = A.size()
@@ -153,12 +186,13 @@ class TCN_GCN_unit(nn.Module):
         super(TCN_GCN_unit, self).__init__()
         num_jpts = A.shape[-1]
         self.gcn1 = unit_gcn(in_channels, out_channels, A, groups, num_point)
-        self.tcn1 = unit_tcn(out_channels, out_channels,
-                             stride=stride, num_point=num_point)
+        self.tcn1 = unit_tcn(
+            out_channels, out_channels, stride=stride, num_point=num_point
+            )
         self.relu = nn.ReLU()
 
         self.A = nn.Parameter(torch.tensor(np.sum(np.reshape(A.astype(np.float32), [
-                              3, num_point, num_point]), axis=0), dtype=torch.float32, requires_grad=False, device='cuda'), requires_grad=False)
+                              3, num_point, num_point]), axis=0), dtype=torch.float32, requires_grad=False), requires_grad=False)
 
         if not residual:
             self.residual = lambda x: 0
@@ -173,7 +207,7 @@ class TCN_GCN_unit(nn.Module):
         self.dropT_skip = DropBlockT_1d(block_size=block_size)
         self.attention = attention
         if attention:
-            print('Attention Enabled!')
+            # print('Attention Enabled!')
             self.sigmoid = nn.Sigmoid()
             # temporal attention
             self.conv_ta = nn.Conv1d(out_channels, 1, 9, padding=4)
@@ -215,7 +249,6 @@ class TCN_GCN_unit(nn.Module):
             se2 = self.sigmoid(self.fc2c(se1))
             y = y * se2.unsqueeze(-1).unsqueeze(-1) + y
             # a3 = se2.unsqueeze(-1).unsqueeze(-1)
-            
         y = self.tcn1(y, keep_prob, self.A)
         x_skip = self.dropT_skip(self.dropSke(self.residual(x), keep_prob, self.A), keep_prob)
         return self.relu(y + x_skip)
@@ -228,37 +261,43 @@ class Model(nn.Module):
         if graph is None:
             raise ValueError()
         else:
-            Graph = import_class(graph)
-            self.graph = Graph(**graph_args)
+            # Graph = import_class(graph)
+            self.graph = graph(**graph_args)
 
-        A = self.graph.A
+        A = np.array(self.graph.A)
+        # logger.info(A)
         self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
+        self.bn_init(self.data_bn, 1)
 
-        self.l1 = TCN_GCN_unit(in_channels, 64, A, groups, num_point,
-                               block_size, residual=False)
+        self.l1 = TCN_GCN_unit(in_channels, 64, A, groups, num_point, block_size, residual=False)
         self.l2 = TCN_GCN_unit(64, 64, A, groups, num_point, block_size)
         self.l3 = TCN_GCN_unit(64, 64, A, groups, num_point, block_size)
         self.l4 = TCN_GCN_unit(64, 64, A, groups, num_point, block_size)
-        self.l5 = TCN_GCN_unit(
-            64, 128, A, groups, num_point, block_size, stride=2)
+        self.l5 = TCN_GCN_unit(64, 128, A, groups, num_point, block_size, stride=2)
         self.l6 = TCN_GCN_unit(128, 128, A, groups, num_point, block_size)
         self.l7 = TCN_GCN_unit(128, 128, A, groups, num_point, block_size)
-        self.l8 = TCN_GCN_unit(128, 256, A, groups,
-                               num_point, block_size, stride=2)
+        self.l8 = TCN_GCN_unit(128, 256, A, groups, num_point, block_size, stride=2)
         self.l9 = TCN_GCN_unit(256, 256, A, groups, num_point, block_size)
         self.l10 = TCN_GCN_unit(256, 256, A, groups, num_point, block_size)
 
         self.fc = nn.Linear(256, num_class)
-        nn.init.normal(self.fc.weight, 0, math.sqrt(2. / num_class))
-        bn_init(self.data_bn, 1)
+        nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
 
-    def forward(self, x, keep_prob=0.9):
+    def conv_init(self, conv):
+        nn.init.kaiming_normal_(conv.weight, mode='fan_out')
+        nn.init.constant_(conv.bias, 0)
+
+    def bn_init(self, bn, scale):
+        nn.init.constant_(bn.weight, scale)
+        nn.init.constant_(bn.bias, 0)
+
+    def feature_extractor(self, x, keep_prob=0.9):
         N, C, T, V, M = x.size()
         x = x.permute(0, 4, 3, 1, 2).contiguous().view(N, M * V * C, T)
         x = self.data_bn(x)
         x = x.view(N, M, V, C, T).permute(
             0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
-
+        logger.info(x.shape)
         x = self.l1(x, 1.0)
         x = self.l2(x, 1.0)
         x = self.l3(x, 1.0)
@@ -269,15 +308,14 @@ class Model(nn.Module):
         x = self.l8(x, keep_prob)
         x = self.l9(x, keep_prob)
         x = self.l10(x, keep_prob)
-
+        logger.info(x.shape)
         # N*M,C,T,V
         c_new = x.size(1)
-
-        # print(x.size())
-        # print(N, M, c_new)
-
-        # x = x.view(N, M, c_new, -1)
         x = x.reshape(N, M, c_new, -1)
         x = x.mean(3).mean(1)
+        return x
 
+    def forward(self, x, keep_prob=0.9):
+        x = self.feature_extractor(x, keep_prob)
+        return x.T.unsqueeze(0)
         return self.fc(x)
